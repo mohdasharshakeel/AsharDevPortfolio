934f2a5b054a84771da3f0938127f0c01fc3b631
mohdasharshakeel <mohdasharshakeel@gmail.com>
2025-07-02 20:13:07 +0530

    portfolio

---
+++ src/sections/Navbar.jsx
@@ -0,0 +1,30 @@
+import React, { useRef } from "react";
+
+const Navbar = () => {
+  const navRef = useRef(null);
+  const linksRef =useRef([])
+
+  return (
+    <nav
+      ref={navRef}
+      className="fixed z-50 flex flex-col justify-between w-full h-full px-10 uppercase bg-black text-white/80 py-28 gap-y-10 md:w-1/2 md:left-1/2"
+    >
+      <div className="flex flex-col text-5xl gap-y-2 md:text-6xl lg:text-8xl">
+        {["home", "services", "about", "work", "contact"].map(
+          (section, index) => (
+            <div key={index} ref={(el)=>(linksRef.current[index]=el)}>
+              <a className="transition-all duration-300 cursor-pointer hover:text-white">
+                {section}
+              </a>
+            </div>
+          )
+        )}
+      </div>
+      <div className="flex flex-col flex-wrap justify-between gap-8 md:flex-row">
+
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
